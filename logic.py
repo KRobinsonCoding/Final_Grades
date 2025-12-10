1256e413e8fcf43ea7f76a4820eef91f2277a7d3
KRobinsonCoding <56876142@nebraska.edu>
2025-12-10 13:50:15 -0600

    added type hinting and docstrings

--- logic.py
+++ logic.py
@@ -4,6 +4,9 @@ from gui import *
 
 class Logic(QMainWindow, Ui_Grade_Window):
     def __init__(self)->None:
+        """
+        Method that sets up grade window and creates grades.csv file
+        """
         super().__init__()
         self.setupUi(self)
         self.validate_attempts()
@@ -14,11 +17,14 @@ class Logic(QMainWindow, Ui_Grade_Window):
         self.input_score4.textChanged.connect(lambda: self.validate_input())
         self.input_student_name.textChanged.connect(lambda: self.validate_input())
         self.button_submit.clicked.connect(lambda: self.submit())
-        with open('grades.csv', 'w', newline='') as csvfile:
+        with open('grades.csv', 'w', newline='') as csvfile: # overwrite or append
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow(['Name', 'Score 1', 'Score 2', 'Score 3', 'Score 4', 'Average'])
 
-    def validate_input(self):
+    def validate_input(self)->None:
+        """
+        Method that makes sure all the inputs are valid, if they are the submit button turns green
+        """
         self.label_error.setText('')
         self.validate_attempts()
         self.validate_scores()
@@ -28,7 +34,10 @@ class Logic(QMainWindow, Ui_Grade_Window):
         else:
             self.button_submit.setStyleSheet("color: black;")
 
-    def validate_attempts(self):
+    def validate_attempts(self)->None:
+        """
+        Method that validates the attempts input
+        """
         if self.input_attempts.text() == '':
             self.hide_scores()
             # should this be accepted as all zeros?
@@ -45,7 +54,10 @@ class Logic(QMainWindow, Ui_Grade_Window):
             self.label_error.setText("Number of attempts should be an\ninteger between 1 and 4")
             self.hide_scores()
 
-    def validate_scores(self):
+    def validate_scores(self)->None:
+        """
+        Method that validates the scores input
+        """
         input_list = [self.input_score1.text(), self.input_score2.text(), self.input_score3.text(),self.input_score4.text()]
         try:
             for score in input_list:
@@ -60,12 +72,19 @@ class Logic(QMainWindow, Ui_Grade_Window):
             self.label_error.setStyleSheet("color: red;")
             self.label_error.setText(f'Grade out of range\n\nScores should to be an \ninteger between 0 and 100')
 
-    def validate_name(self):
+    def validate_name(self)->None:
+        """
+        Method that makes sure the name input is not empty
+        """
         if self.input_student_name.text() == '':
             self.label_error.setStyleSheet("color: red;")
             self.label_error.setText('Student Name cannot be empty')
 
-    def show_scores(self, attempts):
+    def show_scores(self, attempts)->None:
+        """
+        Method that shows the scores input labels and boxes
+        :param attempts: indicates how many of the score labels/boxes should be shown
+        """
         if attempts >= 1:
             self.label_score1.show()
             self.input_score1.show()
@@ -79,7 +98,10 @@ class Logic(QMainWindow, Ui_Grade_Window):
             self.label_score4.show()
             self.input_score4.show()
 
-    def hide_scores(self):
+    def hide_scores(self)->None:
+        """
+        Method that clears and hides the scores input labels and boxes
+        """
         self.label_score1.hide()
         self.input_score1.hide()
         self.label_score2.hide()
@@ -93,7 +115,10 @@ class Logic(QMainWindow, Ui_Grade_Window):
         self.input_score3.setText('')
         self.input_score4.setText('')
 
-    def submit(self):
+    def submit(self)->None:
+        """
+        Method that writes the grades to the csv file and resets the user interface
+        """
         self.validate_input()
         if self.label_error.text() == '':
             input_list = [self.input_score1.text(), self.input_score2.text(), self.input_score3.text(),
